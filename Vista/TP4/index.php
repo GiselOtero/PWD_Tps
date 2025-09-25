e958d84ad75d7342d9e650901e23908d42ed24f8
GiselOtero <gisel.otero@est.fi.uncoma.edu.ar>
2025-09-25 16:42:24 -0300

    Menu Tp4

---
+++ Vista/TP4/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once "../Estructura/cabecera.php";
+
+?>
+<div class="list-group">
+    <a class="list-group-item list-group-item-action" href="ListaAuto.php">Lista Auto</a>
+    <a class="list-group-item list-group-item-action" href="buscarAuto.php">Buscar Auto</a>
+    <a class="list-group-item list-group-item-action" href="formAuto.php">Nuevo Auto</a>
+    <a class="list-group-item list-group-item-action" href="listaPersona.php">Lista Persona</a>
+    <a class="list-group-item list-group-item-action" href="buscarPersona.php">Buscar Persona</a>
+    <a class="list-group-item list-group-item-action" href="formPersona.php">Nueva Persona</a>
+
+   
+</div>
